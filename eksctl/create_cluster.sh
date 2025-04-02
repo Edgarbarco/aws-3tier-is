f5b79618a1d602a4aaf86c52a9b250a6dd9c030b
Edgarbarco <edgarbarco@MacBook-Pro-de-User.local>
2025-04-02 16:27:56 -0600

    Add create_cluster.sh for EKS cluster deployment

---
+++ eksctl/create_cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+eksctl create cluster \
+  --name umgis2025 \
+  --region us-east-1 \
+  --nodegroup-name standard-workers \
+  --node-type t3.medium \
+  --nodes 2 \
+  --nodes-min 1 \
+  --nodes-max 3 \
+  --managed
+
+
+
